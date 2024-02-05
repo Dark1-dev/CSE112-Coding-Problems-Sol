f0983140bc7ae55231a7d646104be8762f2c2eac
Dark1-dev <shansarkar272@gmail.com>
2024-02-05 23:01:26 +0600

    Update 1.cpp

--- Lab Class 4 Sol/1.cpp
+++ Lab Class 4 Sol/1.cpp
@@ -2,9 +2,39 @@
 
 using namespace std;
 
-int main()
-{
+int add(int a, int b) {
+    return a + b;
+}
 
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+double divide(int a, int b) {
+    if (b != 0)
+        return a / b;
+}
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter the first integer: ";
+    cin >> num1;
+
+    cout << "Enter the second integer: ";
+    cin >> num2;
+
+    cout << "\nSum: " << add(num1, num2) << endl;
+    cout << "Subtraction: " << subtract(num1, num2) << endl;
+    cout << "Multiplication: " << multiply(num1, num2) << endl;
+
+    if (num2 != 0) {
+        cout << "Division: " << divide(num1, num2) << endl;
+    }
 
     return 0;
 }
