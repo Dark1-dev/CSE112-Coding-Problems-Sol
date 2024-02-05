0eed5f0a208d5d32130d3f0767b840abe09170a3
Dark1-dev <shansarkar272@gmail.com>
2024-02-05 23:09:20 +0600

    Update Untitled5.cpp

--- Lab Class 3 Sol/Untitled5.cpp
+++ Lab Class 3 Sol/Untitled5.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 
-using namespace std;
+namespace std;
 
-int main()
-{
+int fact(int n) {
+    return (n == 0 || n == 1) ? 1 : n * fact(n - 1);
+}
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num >= 0) {
+        cout << "Factorial: " << fact(num) << "\n";
 
     return 0;
 }
-
